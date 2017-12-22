a7fdb0b4c197c31cbdf5ea78a77aeeef85a19f2d
Jan Erik Bertram <bertram@jeb.de>
2017-12-22 14:17:32 +0100

    Initial commit, frist draft of defuse component

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Defuse/>
+  </div>
+</template>
+
+<script>
+import Defuse from './components/Defuse'
+
+export default {
+  name: 'app',
+  components: {
+    Defuse
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Defuse.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="defuse">
+  <div class="row" v-for="row,idx in map">
+    <div class="field" v-for="field in row" @click="open">
+        <span v-if="field.isBomb">    💣</span>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+function Field (x, y) {
+  this.x = x
+  this.y = y
+  this.isOpen = false
+  this.isBomb = false
+  this.isMarked = false
+}
+
+export default {
+  name: 'Defuse',
+  data () {
+    return {
+      dimX: 10,
+      dimY: 10,
+      map: null,
+      numberOfBombs: null,
+      remainingBombs: null
+    }
+  },
+  mounted () {
+    this.clearMap()
+    this.placeBombs()
+  },
+  methods: {
+    clearMap () {
+      this.map = []
+      for (let y = 0; y < this.dimY; y++) {
+        let row = []
+        for (let x = 0; x < this.dimX; x++) {
+          let field = new Field(x, y)
+          row.push(field)
+        }
+        this.map.push(row)
+      }
+    },
+
+    placeBombs () {
+      this.map[1][1].isBomb = true
+    },
+
+    open () {
+      console.log(arguments)
+      //
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+:root {
+  --fieldwidth: 20px;
+}
+
+.field {
+  // height: var(--fieldwidth);
+  // width: var(--fieldwidth);
+  height: 20px;
+  width: 20px;
+  background-color: #ddd;
+  margin: 1px;
+}
+
+.row {
+  display: flex;
+}
+</style>
