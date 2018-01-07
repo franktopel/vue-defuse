ecd49257cc49179c4b50844e7b6b3d9c07fd14ab
Frank Topel <frank.topel@rewe-digital.com>
2018-01-07 15:18:33 +0100

    disable text selection for playfield to prevent text markers on mobile showing up

--- src/components/Defuse.vue
+++ src/components/Defuse.vue
@@ -302,6 +302,13 @@ export default {
 
   .playfield {
     position: relative;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
   }
 
   .playfield.game-over {
